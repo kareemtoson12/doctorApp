c37e094eeaf885f1679424b7db25de2e1165b9c4
kareemtoson12 <kareemtoson1@gmail.com>
2024-08-28 00:44:32 +0300

    onboarding done

--- doctor_app/lib/features/onboarding/onboarding_screen.dart
+++ doctor_app/lib/features/onboarding/onboarding_screen.dart
@@ -1,4 +1,10 @@
+import 'package:doctor_app/core/theming/styels.dart';
+import 'package:doctor_app/features/onboarding/widgets/doc_logo_and_name.dart';
+import 'package:doctor_app/features/onboarding/widgets/started_bottom.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'widgets/DoctorImageAndText.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -6,7 +12,38 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('onbarding screen')),
+      body: SafeArea(
+          child: SingleChildScrollView(
+              child: Padding(
+        padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+        child: Column(
+          children: [
+            DocLogoAndNamae(),
+            SizedBox(
+              height: 30.h,
+            ),
+            DoctorImageAndText(context),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 30.w,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Manage and schedule all of your medical appointments easily with\n Docdoc to get a new experience.',
+                    style: CustomstextStyels.font13RegularGray,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 30.h,
+                  ),
+                  getstartedBottom(context),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ))),
     );
   }
 }
---
+++ doctor_app/lib/features/onboarding/widgets/started_bottom.dart
@@ -0,0 +1,27 @@
+import 'package:doctor_app/core/helpers/extenstions.dart';
+import 'package:doctor_app/core/routing/routes.dart';
+import 'package:doctor_app/core/theming/colors.dart';
+import 'package:doctor_app/core/theming/styels.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+Widget getstartedBottom(BuildContext context) {
+  // ignore: prefer_const_constructors
+  return TextButton(
+      onPressed: () {
+        context.pushNamed(Routes.loginScreen, arguments: {'id': 1});
+      },
+      child: Container(
+          padding: EdgeInsets.all(15.h),
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: ColorsManger.minBlue,
+              borderRadius: BorderRadius.circular(50)),
+          child: Center(
+            child: Text(
+              'Get Started',
+              style: CustomstextStyels.font24Withet700Wight,
+            ),
+          )));
+}
